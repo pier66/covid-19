10494158099feb5a8d2ffb514479e739826eb511
Pierre Obertin <aquarius@pt.lu>
2020-06-07 10:07:33 +0200

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,160 @@
+
+// date functions **************************************************************
+
+function dateStrToObj( dateStr ){
+  return new Date(
+    parseInt( dateStr.substr(6,4) ),
+    parseInt( dateStr.substr(3,2) )-1,
+    parseInt( dateStr.substr(0,2) )
+  );
+};
+
+function dateObjToStr( dateObj ){
+  var str = String( dateObj.getDate() ).padStart(2, '0') + "/";
+  str += String( dateObj.getMonth() + 1 ).padStart(2, '0') + "/";
+  str += String( dateObj.getFullYear() );
+  return str;
+};
+
+// data functions **************************************************************
+
+function translateValuesByDays( varName, byDays ){
+  var translatedValues = {};
+  for( var i in varName ){
+    var date = parseInt(i.substr(0,2));
+    var month = parseInt(i.substr(3,2))-1;
+    var year = parseInt(i.substr(6,4));
+    var curDate = new Date( year, month, date );
+    var newDate = curDate;
+    newDate.setDate( date + byDays );
+    var newDateString = String( newDate.getDate()).padStart(2, '0');
+    newDateString += "/" + String( newDate.getMonth()+1 ).padStart(2, '0');
+    newDateString += "/" + String( newDate.getFullYear() );
+    translatedValues[newDateString] = varName[i];
+  };
+  return translatedValues;
+};
+
+function getValuesReadyToPlot( varName ){
+  var valuesReadyToPlot = [];
+  for( var i = 0; i < displayDates.length; i++ ) {
+    var dateStr = dateObjToStr( displayDates[i] );
+    if( varName[dateStr] ){
+      valuesReadyToPlot.push( varName[dateStr] );
+    }
+    else {
+      valuesReadyToPlot.push( 0 );
+    }
+  }
+  return valuesReadyToPlot;
+};
+
+function smoothValues( values ){
+  var smoothedValues = [];
+  for( var i = 0; i < values.length; i++ ){
+    if( i == 0 || i == values.length-1 ) {
+      smoothedValues.push( values[i] );
+    } else if( i == 1 || i == values.length-2 ) {
+      smoothedValues.push( ( values[i-1] + values[i] + values[i+1] ) / 3 );
+    } else if( i == 2 || i == values.length-3 ) {
+       var sum = 0;
+       for( var j = -2; j <= 2; j++ ){
+         sum += values[i+j];
+       };
+       smoothedValues.push( sum / 5 );
+    } else {
+      var sum = 0;
+      for( var j = -3; j <= 3; j++ ){
+        sum += values[i+j];
+      };
+      smoothedValues.push( sum / 7 );
+    }
+  }
+  return smoothedValues;
+};
+
+function multiplyValues( values, factor ) {
+  var multipliedValues = [];
+  for( var i = 0; i < values.length; i++ ){
+    multipliedValues[i] = factor * values[i];
+  };
+  return multipliedValues;
+};
+
+function cumulateValues( values ) {
+  var sum = 0;
+  var cumulatedValues = [];
+  for( var i = 0; i < values.length; i++ ){
+    sum += values[i];
+    cumulatedValues[i] = sum;
+  };
+  return cumulatedValues;
+};
+
+function addValues( values1, values2 ){
+  var addedValues = [];
+  for( var i = 0; i < values1.length; i++ ){
+    addedValues[i] = values1[i] + values2[i];
+  };
+  return addedValues;
+};
+
+function divideValues( values1, values2 ){
+  var dividedValues = [];
+  for( var i = 0; i < values1.length; i++ ){
+    dividedValues[i] = values1[i] / values2[i];
+  };
+  return dividedValues;
+};
+
+
+
+/******************************************************************************
+
+var S_lockDown = {};
+var I_lockDown = {};
+var R_lockDown = {};
+
+function beta_lockDown( dateObj ) {
+  if( dateObj < dateStrToObj( '18/03/2020' ) ){
+    return 0.37;
+  }
+  else if( dateObj < dateStrToObj( '25/03/2020' ) ){
+    return 0.33;
+  }
+  else {
+    return 0.29;
+  }
+};
+
+function SIR_lockDown() {
+  // count days
+  var diffDays = Math.round( Math.abs( ( endDateObj - startDateObj ) / (24 * 60 * 60 * 1000) ) );
+  // console.log( diffDays );
+  var steps = diffDays * stepsPerDay;
+
+  var S2 = [];
+  var I2 = [];
+  var R2 = [];
+
+  S2[0] = N;
+  I2[0] = I_0 * N;
+  R2[0] = 0;
+
+  var i = 0;
+  for( var d = 0; d < displayDates.length; d++ ) {
+    // console.log(d);
+    S_lockDown[dateObjToStr(displayDates[d])] = S2[i];
+    I_lockDown[dateObjToStr(displayDates[d])] = I2[i];
+    R_lockDown[dateObjToStr(displayDates[d])] = R2[i];
+
+    for( var s = 1; s <= stepsPerDay; s++ ){
+      S2[i+1] = S2[i] - beta_lockDown(displayDates[d]) * S2[i] * I2[i] / stepsPerDay;
+      I2[i+1] = I2[i] + ( beta_lockDown(displayDates[d]) * S2[i] * I2[i] - gamma * I2[i] ) / stepsPerDay;
+      R2[i+1] = R2[i] + gamma * I2[i] / stepsPerDay;
+      i++;
+    }
+  }
+};
+
+**/
---
+++ js/main.js
@@ -0,0 +1,407 @@
+
+// html tag
+const graphDiv = document.getElementById('graphDiv');
+
+// dates *********************************************
+const startDateStr = "24/02/2020";
+const endDateStr = "31/05/2020";
+
+const startDateObj = dateStrToObj( startDateStr );
+const endDateObj = dateStrToObj( endDateStr );
+
+var displayDates = [];
+var startDateObjFor = dateStrToObj( startDateStr );
+for( var d = startDateObjFor; d <= endDateObj; d.setDate( d.getDate() + 1 )) {
+    displayDates.push( new Date(d) );
+};
+
+// SIR model *******************************************************************
+// config ************************
+const sirStepsPerDay = 10;
+
+var sirN = 5000;
+var sirI_0 = 0.002;
+var sirR_0 = 0;
+
+var sirBeta = 0.33;
+var sirGamma = 1/14;
+
+var sirRepro_0 = sirBeta / sirGamma;
+// *******************************
+
+var sirS = {};
+var sirI = {};
+var sirR = {};
+var sirIperDay = {};
+var sirRperDay = {};
+
+var sStep = [];
+var iStep = [];
+var rStep = [];
+
+function SIR(){
+  sirS = {};
+  sirI = {};
+  sirR = {};
+
+  sirIperDay = {};
+  sirRperDay = {};
+
+  sStep = [];
+  iStep = [];
+  rStep = [];
+
+  sirRepro_0 = sirBeta / ( sirN * sirGamma );
+
+  sStep[0] = sirN;
+  iStep[0] = sirI_0 * sirN;
+  rStep[0] = sirR_0;
+
+  var i = 0;
+  for( var d = 0; d < displayDates.length; d++ ) {
+
+    sirS[dateObjToStr(displayDates[d])] = sStep[i];
+    sirI[dateObjToStr(displayDates[d])] = iStep[i];
+    sirR[dateObjToStr(displayDates[d])] = rStep[i];
+
+    for( var s = 1; s <= sirStepsPerDay; s++ ){
+      sStep[i+1] = sStep[i] - ( sirBeta / sirN ) * sStep[i] * iStep[i] / sirStepsPerDay;
+      iStep[i+1] = iStep[i] + ( ( sirBeta / sirN ) * sStep[i] * iStep[i] - sirGamma * iStep[i] ) / sirStepsPerDay;
+      rStep[i+1] = rStep[i] + sirGamma * iStep[i] / sirStepsPerDay;
+      i++;
+    };
+
+    sirIperDay[dateObjToStr(displayDates[d])] = - ( sStep[i-1] - sirS[dateObjToStr(displayDates[d])] );
+    sirRperDay[dateObjToStr(displayDates[d])] = - ( rStep[i-1] - sirR[dateObjToStr(displayDates[d])] );
+
+  }
+};
+
+var sirSdde = {};
+var sirIdde = {};
+var sirRdde = {};
+var sirIddePerDay = {};
+var sirRddePerDay = {};
+var c = 0.001;
+
+function SIR_DDE(){
+  sirSdde = {};
+  sirIdde = {};
+  sirRdde = {};
+  sirIddePerDay = {};
+  sirRddePerDay = {};
+
+  sirRepro_0 = sirBeta / sirGamma;
+
+  var capGamma = - gsl_sf_lambert_W0( - sirRepro_0 * Math.exp( -sirRepro_0 ) ) / sirRepro_0;
+
+  for( var d = 0; d < displayDates.length; d++ ) {
+
+    var x = Math.exp( sirBeta * ( 1 - capGamma ) * d );
+
+    sirSdde[dateObjToStr(displayDates[d])] = sirN *
+      ( Math.pow( 1 - capGamma, 2 ) + capGamma * c * x )
+      /
+      ( Math.pow( 1 - capGamma, 2 ) + c * x );
+
+    sirIdde[dateObjToStr(displayDates[d])] = sirN *
+      ( Math.pow( 1 - capGamma, 4 ) * c * x )
+      /
+      (
+        ( Math.pow( 1 - capGamma, 2 ) + c * x )
+        *
+        ( Math.pow( 1 - capGamma, 2 ) + capGamma * c * x )
+      );
+
+    sirRdde[dateObjToStr(displayDates[d])] = sirN *
+      ( capGamma * ( 1 - capGamma ) * c * x )
+      /
+      ( Math.pow( 1 - capGamma, 2 ) + capGamma * c * x );
+  };
+
+}
+
+// plot ************************************************************************
+
+SIR();
+
+//v SIR_DDE();
+
+var detectedPerCentPerDay =
+    multiplyValues(
+      divideValues(
+        getValuesReadyToPlot( covidPlusPerDay ),
+        getValuesReadyToPlot( testedPerDay )
+      ), 100
+    );
+
+
+var infected =
+    cumulateValues (
+      addValues(
+        addValues(
+          getValuesReadyToPlot( translateValuesByDays( covidPlusPerDay, -8 ) ),
+          multiplyValues( getValuesReadyToPlot( translateValuesByDays( recoveredPerDay, -8 ) ) , -1 )
+        ),
+        multiplyValues( getValuesReadyToPlot( gonePerDay ), -1 )
+      )
+    );
+
+var infectedSmoothed =
+      smoothValues(
+        cumulateValues (
+          addValues(
+            addValues(
+              getValuesReadyToPlot( translateValuesByDays( covidPlusPerDay, -8 ) ),
+              multiplyValues( getValuesReadyToPlot( translateValuesByDays( recoveredPerDay, -8 ) ) , -1 )
+            ),
+            multiplyValues( getValuesReadyToPlot( gonePerDay ), -1 )
+          )
+        )
+      );
+
+var deltaBetaLockdown =
+    multiplyValues(
+      addValues(
+        infectedSmoothed,                                   // infected smoothed
+        multiplyValues( getValuesReadyToPlot( sirI ), -1 )  // -I
+      ), 1 / ( sirStepsPerDay  ) );  // * sirFactor
+
+
+/*
+'#1f77b4',  // muted blue
+'#ff7f0e',  // safety orange
+'#2ca02c',  // cooked asparagus green
+'#d62728',  // brick red
+'#9467bd',  // muted purple
+'#8c564b',  // chestnut brown
+'#e377c2',  // raspberry yogurt pink
+'#7f7f7f',  // middle gray
+'#bcbd22',  // curry yellow-green
+'#17becf'   // blue-teal
+      */
+
+
+Plotly.newPlot(
+  graphDiv,
+  [
+  {
+    name: 'detected per day',
+    marker: {color: '#d62728'},
+  	x: displayDates,
+  	y: getValuesReadyToPlot( covidPlusPerDay ),
+    type: 'bar'
+  },
+  {
+    name: 'tested per day',
+    marker: {color: '#bbbbbb'},
+  	x: displayDates,
+  	y: getValuesReadyToPlot( testedPerDay ),
+    type: 'bar'
+  },
+  {
+    name: '% detected per day',
+    marker: {color: '#ff7f0e'},
+    x: displayDates,
+    y: detectedPerCentPerDay,
+    type: 'bar'
+  },
+  {
+    name: 'infected per day',
+    marker: {color: '#8c564b'},
+  	x: displayDates,
+  	y: getValuesReadyToPlot( translateValuesByDays( covidPlusPerDay, -8 ) ),
+    type: 'bar'
+  },
+
+  {
+    name: 'infected per day smoothed',
+    marker: {color: '#d62728'},
+    x: displayDates,
+    y: smoothValues( getValuesReadyToPlot( translateValuesByDays( covidPlusPerDay, -8 ) ) ),
+    type: 'bar'
+  },
+  {
+    name: 'recovered per day',
+    marker: {color: '#2ca02c'},
+  	x: displayDates,
+  	y: multiplyValues( getValuesReadyToPlot( recoveredPerDay ), -1 ),
+    type: 'bar'
+  },
+  {
+    name: 'recovered per day -8 days',
+    marker: {color: '#bcbd22'},
+    x: displayDates,
+    y: multiplyValues( getValuesReadyToPlot( translateValuesByDays( recoveredPerDay, -8 ) ), -1),
+    type: 'bar',
+    barmode: 'stack',
+    xaxis: 'x1'
+  },
+  {
+    name: 'recovered per day -8 days smoothed',
+    marker: {color: '#2ca02c'},
+    x: displayDates,
+    y: multiplyValues( smoothValues( getValuesReadyToPlot( translateValuesByDays( recoveredPerDay, -8 ) ) ), -1),
+    type: 'bar'
+  },
+  {
+    name: 'lethal cases per day',
+    marker: {color: '#1f77b4'},
+    x: displayDates,
+    y: multiplyValues( getValuesReadyToPlot( gonePerDay ), -1 ),
+    type: 'bar',
+    barmode: 'stack',
+    xaxis: 'x1'
+  },
+  {
+    name: 'infected',
+    marker: {color: '#e377c2'},
+    x: displayDates,
+    y: infected,
+    type: 'bar'
+  },
+  {
+    name: 'infected smoothed',
+    marker: {color: '#9467bd'},
+    x: displayDates,
+    y: infectedSmoothed,
+    type: 'bar'
+  },
+  {
+    name: 'SIR susceptible',
+    marker: {color: '#17becf'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirS )
+  },
+  {
+    name: 'SIR infected',
+    marker: {color: '#9467bd'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirI )
+  },
+  {
+    name: 'SIR recovered',
+    marker: {color: '#bcbd22'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirR )
+  },
+  {
+    name: 'SIR infected per day',
+    marker: {color: '#d62728'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirIperDay )
+  },
+  {
+    name: 'SIR recovered per day',
+    marker: {color: '#2ca02c'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirRperDay )
+  } /*,
+  {
+    name: 'SIR DDE susceptible',
+    marker: {color: '#17becf'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirSdde )
+  },
+  {
+    name: 'SIR DDE infected',
+    marker: {color: '#9467bd'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirIdde )
+  },
+  {
+    name: 'SIR DDE recovered',
+    marker: {color: '#bcbd22'},
+    x: displayDates,
+    y: getValuesReadyToPlot( sirRdde )
+  }*/
+],
+{
+  margin: { t: 20 } }
+);
+
+
+function updateGraph(){
+
+  Plotly.deleteTraces( graphDiv, [/*-8,-7,-6,*/-5,-4,-3, -2, -1] );
+
+  SIR();
+
+  SIR_DDE();
+
+  var data = [
+    {
+      name: 'SIR susceptible',
+      marker: {color: '#17becf'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirS )
+    },
+    {
+      name: 'SIR infected',
+      marker: {color: '#9467bd'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirI )
+    },
+    {
+      name: 'SIR recovered',
+      marker: {color: '#bcbd22'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirR )
+    },
+    {
+      name: 'SIR infected per day',
+      marker: {color: '#d62728'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirIperDay )
+    },
+    {
+      name: 'SIR recovered per day',
+      marker: {color: '#2ca02c'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirRperDay )
+    }/*,
+    {
+      name: 'SIR DDE susceptible',
+      marker: {color: '#17becf'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirSdde )
+    },
+    {
+      name: 'SIR DDE infected',
+      marker: {color: '#9467bd'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirIdde )
+    },
+    {
+      name: 'SIR DDE recovered',
+      marker: {color: '#bcbd22'},
+      x: displayDates,
+      y: getValuesReadyToPlot( sirRdde )
+    } */
+] ;
+  Plotly.addTraces( graphDiv, data );
+};
+
+function updateN(val) {
+    sirN = parseFloat(val);
+    document.getElementById('textN').value = val;
+    updateGraph();
+};
+
+function updateI_0(val) {
+    sirI_0 = parseFloat(val);
+    document.getElementById('textI_0').value = val;
+    updateGraph();
+};
+
+function updateBeta(val) {
+    sirBeta = parseFloat(val);
+    document.getElementById('textBeta').value = val;
+    updateGraph();
+};
+
+function updateGamma(val) {
+    sirGamma = 1 / parseFloat(val);
+    document.getElementById('textGamma').value = val;
+    updateGraph();
+};
